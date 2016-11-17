79bcb6700a61b939ca8ab564ea06d4a5fbbfce46
sharipkanov <sharipkanov@gmail.com>
2016-11-17 12:22:00 +0600

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,16 @@
+(function() {
+
+function CINDRELLAPROJECT() {
+	var _self = this;
+
+	_self.constructor();
+}
+
+CINDRELLAPROJECT.prototype.constructor = function() {
+	console.log('This is a constructor function...');
+}
+
+var app = new CINDRELLAPROJECT;
+
+
+})();
